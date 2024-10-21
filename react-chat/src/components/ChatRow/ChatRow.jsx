4605b629cf3c24fc39489489677e7e371cb0aaaa
gudzan <andreeva.anva@gmail.com>
2024-10-21 21:16:04 +0300

    dz5 | fix

--- react-chat/src/components/ChatRow/ChatRow.jsx
+++ react-chat/src/components/ChatRow/ChatRow.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import "./ChatRow.scss"
 import { usePage } from "../../hooks/usePage";
 import DeliveredIcon from "../DliveredIcon/DeliveredIcon";
 import { transformDate } from "../../utils";
+import "./ChatRow.scss"
 
 const ChatRow = ({ chat, isNew }) => {
   const { setChatId } = usePage()
