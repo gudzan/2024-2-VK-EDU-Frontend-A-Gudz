50d42eb0ea328d1d3e0002f4b9dc4506fb8e9b49
gudzan <andreeva.anva@gmail.com>
2024-12-23 21:19:46 +0300

    exam | add history

--- exam/src/components/HistoryItems/HistoryItems.tsx
+++ exam/src/components/HistoryItems/HistoryItems.tsx
@@ -1,23 +1,35 @@
 import { useAppSelector } from "../../redux/store";
 import { TranslationData } from "../../types/translationData";
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import styles from "./HistoryItems.module.scss"
 
 const HistoryItems = () => {
   const history: TranslationData[] = useAppSelector(
     (state) => state.translates
   );
-  console.log(history);
 
+  if (history.length === 0) {
+    return (
+      <>
+        <p>История пуста</p>
+      </>
+    )
+  }
 
   return (
-    <div>
+    <div className={styles.items}>
       {history.map((item) =>
-        <>
-          <p>{item.languageFrom}</p><ArrowForwardIcon/>
-          <p>{item.languageTo}</p>
-          <p>{item.textFrom}</p>
-          <p>{item.textTo}</p>
-        </>
+        <div className={styles.item}>
+          <div className={styles.languages}>
+            <p>{item.languageFrom}</p>
+            <ArrowForwardIcon />
+            <p>{item.languageTo}</p>
+          </div>
+          <div className={styles.text}>
+            <p className={styles.textFrom}>{item.textFrom}</p>
+            <p className={styles.textTo}>{item.textTo}</p>
+          </div>
+        </div>
       )}
 
     </div>
